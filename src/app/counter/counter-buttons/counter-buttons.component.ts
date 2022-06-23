00e2a75214d422960f9f02ac6b5ed9e58f3dc233
Fayaz Basheer <pnfayaz@gmail.com>
2022-06-23 20:24:57 +0400

    counter application working state angular 14

--- src/app/counter/counter-buttons/counter-buttons.component.ts
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-counter-buttons',
   templateUrl: './counter-buttons.component.html',
-  styleUrls: ['./counter-buttons.component.css'],
+  styleUrls: ['./counter-buttons.component.scss'],
 })
 export class CounterButtonsComponent implements OnInit {
   constructor(private store: Store<{ counter: { counter: number } }>) {}
--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
-  styleUrls: ['./counter-output.component.css'],
+  styleUrls: ['./counter-output.component.scss'],
 })
 export class CounterOutputComponent implements OnInit {
   counter: number = 0;
